252928e3224ccd668d9398a183586996628de287
llr392425595 <392425595@qq.com>
2018-04-10 16:03:55 +0800

    remove wrong test files

--- src/test/java/com/tw/taskSpringBoot/controllers/SpringApplicationConfiguration.java
+++
@@ -1,8 +0,0 @@
-package com.tw.taskSpringBoot.controllers;
-
-import org.springframework.mock.web.MockServletContext;
-
-public @interface SpringApplicationConfiguration {
-
-  Class<MockServletContext> classes();
-}
